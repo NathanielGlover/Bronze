aae86f3d60291fae0489cc8106e8b1fed4f145f5
Nathaniel Glover <nathanielsglover@gmail.com>
2018-04-22 12:21:02 -0400

    Initial Commit

---
+++ Examples/src/ExampleWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examples
+{
+    internal class ExampleWindow
+    {
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
