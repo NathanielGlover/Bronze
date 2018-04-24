cbdb26e222ccac8a7908c81bb4891954c38dfd9c
Nathaniel Glover <nathanielsglover@gmail.com>
2018-04-24 16:18:09 -0400

    Add simple Initialize function for initializing OpenTK and GLFW

---
+++ Bronze/src/Core/InitializationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bronze.Core
+{
+    public class InitializationException : Exception
+    {
+        public InitializationException(string message) : base(message)
+        {
+            
+        }
+    }
+}
