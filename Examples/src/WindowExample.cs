e5a91ca60b3f0b4672655e9b8de54efb5f023c52
Nathaniel Glover <nathanielsglover@gmail.com>
2018-07-14 19:53:32 -0400

    Update WindowExample to include basic window creation, window events and event processing

---
+++ Examples/src/WindowExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Bronze.UserInterface;
+
+namespace Examples
+{
+    internal class WindowExample
+    {
+        public static void Main(string[] args)
+        {
+            var window = new Window(1300, 650, "Window");
+
+            window.Closed += sender => Console.WriteLine($"Window \"{sender.Title}\" has been closed.");
+
+            window.Focused += (sender, focused) => Console.WriteLine($"Window \"{sender.Title}\" has " + (focused ? "gained" : "lost") + " focus.");
+
+            window.Moved += (sender, location) => Console.WriteLine($"Window \"{sender.Title}\" has moved to {location}.");
+
+            window.Resized += (sender, size) => Console.WriteLine($"Window \"{sender.Title}\" has been resized to {size.X} by {size.Y}");
+
+            window.Minimized += (sender, minimized) =>
+                Console.WriteLine($"Window \"{sender.Title}\" has been " + (minimized ? "minimized." : "restored."));
+
+            window.FilesDropped += (sender, paths) =>
+            {
+                Console.WriteLine($"Window \"{sender.Title}\" has received files:");
+                foreach(string path in paths)
+                {
+                    Console.WriteLine(path);
+                }
+            };
+            
+            while(window.IsOpen)
+            {
+                window.Clear();
+                window.SwapBuffers();
+                
+                Window.WaitEvents();
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
